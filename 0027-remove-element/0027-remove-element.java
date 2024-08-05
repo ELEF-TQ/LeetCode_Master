d90fed4091947987a76de532a8c1ab74fa8082fc
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-08-05 21:09:54 +0100

    Time: 0 ms (100%), Space: 41.8 MB (61.42%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int valid_size = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[valid_size] = nums[i];
+                valid_size++;
+            }
+        }
+
+        return valid_size;
+    }
+}
