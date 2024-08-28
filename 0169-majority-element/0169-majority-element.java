1f251ab69d662dc757595f36bd3513979871b55c
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-08-28 12:37:22 +0100

    Time: 17 ms (14.12%), Space: 49.4 MB (82.3%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        int majorityCount = nums.length / 2;
+
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            if (countMap.get(num) > majorityCount) {
+                return num;
+            }
+        }
+
+        return -1; 
+    }
+}
