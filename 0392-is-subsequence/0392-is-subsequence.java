02370ed64b3ba488b7b52838278403eb487ad1ad
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-09-09 17:31:45 +0100

    Time: 2 ms (58.47%), Space: 41.4 MB (58.69%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sPointer = 0 ;
+        int tPointer = 0 ;
+
+
+        while(tPointer < t.length()){
+            if(sPointer == s.length()) return true;
+            if(s.charAt(sPointer) == t.charAt(tPointer)){
+                sPointer++ ;
+                tPointer++ ;
+            }else {
+                tPointer++ ;
+            }
+
+
+        }
+        return sPointer == s.length() ;
+    }
+}
