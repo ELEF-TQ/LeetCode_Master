462dca665c3270d15f76c3d29ba06873bacbf647
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-09-13 22:18:47 +0100

    Time: 0 ms (100%), Space: 44.2 MB (65.55%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,17 +1,36 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
-
-        HashSet<ListNode> nodes_set = new HashSet<>();
-        
-        while (head != null) {
-            if (nodes_set.contains(head)) {
-                return true;
-            }
-            nodes_set.add(head);
-            head = head.next;
+        if(head == null || head.next == null){
+            return false ;
         }
-        
-        // If we reach the end of the list, there is no cycle
-        return false;
+
+        // using Floyd's Cycle Detection Algorithm
+        ListNode slow = head ; /* tortoise */
+        ListNode fast = head ; /* hare */
+
+        while(fast != null && fast.next != null){
+            slow = slow.next ;
+            fast = fast.next.next ;
+
+            if(slow == fast){
+                return true ;
+            }
+        }
+
+        // if we reach the end
+        return false ;
+
     }
 }
+
+
+/* using a hashset will take O(n) space Complexity : in worst case all the linked list will be stored in the hashset  */
+        // HashSet<ListNode> nodes_set = new HashSet<>();
+        // while (head != null) {
+        //     if (nodes_set.contains(head)) {
+        //         return true;
+        //     }
+        //     nodes_set.add(head);
+        //     head = head.next;
+        // }
+        // return false;
