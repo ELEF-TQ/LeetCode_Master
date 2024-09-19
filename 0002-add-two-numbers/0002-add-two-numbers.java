128146d6c254c41cdc7cab5cf8a7a04c4ec2f9d1
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-09-19 12:11:55 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -7,7 +7,7 @@ class Solution {
         while (p != null || q != null) {
             int x = (p != null) ? p.val : 0;
             int y = (q != null) ? q.val : 0;
-            int sum = carry + x + y;
+            int sum = carry + x + y;.
             carry = sum / 10;
             current.next = new ListNode(sum % 10);
             current = current.next;
