d437b2b99d681cd4d24c380a70d1586fbbc4450f
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-09-20 14:40:07 +0100

    Time: 17 ms (73.86%), Space: 44.7 MB (61.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
-
-        if(s.length() < 2) return s ;
+        if (s.length() < 2) return s;
 
         String maxStr = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= 1; j++) {
+                int left = i, right = i + j; // Check both odd and even length palindromes
+                while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                    left--;
+                    right++;
+                }
 
-        for(int i=0 ; i < s.length() ; i++){
-            String odd = expandFromCenter(s,i,i);
-            String even = expandFromCenter(s,i,i+1);
-            String longer = odd.length() > even.length() ? odd : even ;
-
-            if(longer.length() > maxStr.length()){
-                maxStr = longer ;
+                // left and right have moved one extra step outside
+                if (right - left - 1 > maxStr.length()) {
+                    maxStr = s.substring(left + 1, right);
+                }
             }
         }
-
-        return maxStr ;
-
-    }
-
-    private String expandFromCenter(String s , int left , int right){
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            left-- ;
-            right++ ;
-        }
-
-        // left and right are out of bounds (we start in left+1 and we end in right(cause substring excludes the end index))
-        return s.substring(left+1 ,right);
+        
+        return maxStr;
     }
 }
