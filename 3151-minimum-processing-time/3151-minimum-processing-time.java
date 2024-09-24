9a78c1408c5660b8507284e6c0fb5cac327918f0
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-09-24 20:59:28 +0100

    Time: 80 ms (23.11%), Space: 58.9 MB (86.36%) - LeetHub

--- 3151-minimum-processing-time/3151-minimum-processing-time.java
+++ 3151-minimum-processing-time/3151-minimum-processing-time.java
@@ -4,18 +4,17 @@ class Solution {
     public int minProcessingTime(List<Integer> processorTime, List<Integer> tasks) {
         Collections.sort(processorTime);  
         Collections.sort(tasks, Collections.reverseOrder());
-        
+
         int maxTime = 0;
 
-
-        for (int i = 0 , j=0; i < processorTime.size(); i++ , j+=4) {
-            int processorAvailableTime = processorTime.get(i);
-            int currentMaxTaskTime = processorAvailableTime + tasks.get(j) ; 
+        for (int i = 0, j = 0; i < processorTime.size() && j + 3 < tasks.size(); i++, j += 4) {
             
-            maxTime = Math.max(maxTime, currentMaxTaskTime); 
-     
+            int processorAvailableTime = processorTime.get(i);
+            int currentMaxTaskTime = processorAvailableTime + tasks.get(j);
+
+            maxTime = Math.max(maxTime, currentMaxTaskTime);
         }
-        
+
         return maxTime;
     }
 }
