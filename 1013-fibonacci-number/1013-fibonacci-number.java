25d52da3ac7c28e687ec3653ed8e55a3f0c7c8eb
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-09-26 21:58:32 +0100

    Time: 0 ms (100%), Space: 40.9 MB (7.19%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -3,7 +3,23 @@ class Solution {
 
         if(n==0) return 0 ;
         if(n==1) return 1 ;
-        return fib(n-1) + fib(n-2); 
+        int a=0 ;
+        int b=1;
+        for(int i=2 ; i <= n ; i++){
+            int sum = a + b ;
+            a = b ;
+            b = sum ;
+        }
+        return b ;
+
+       
         
     }
 }
+
+
+
+/*  recursion O(2^n) */
+//  if(n==0) return 0 ;
+//  if(n==1) return 1 ;
+//  return fib(n-1) + fib(n-2); 
