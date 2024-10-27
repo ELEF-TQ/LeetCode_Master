440ec8ec4335881dd333b9ea7c323c6534dfd57d
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-10-28 00:43:35 +0100

    Time: 1 ms (100%), Space: 42.9 MB (24.5%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.java
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.java
@@ -1,23 +1,23 @@
 class Solution {
     public int findPermutationDifference(String s, String t) {
+        int diff = 0;
+        int n = s.length();
 
-        int diff = 0 ;
-        int n = s.length() ;
-        
+    
+        int[] sIndex = new int[26];
+        int[] tIndex = new int[26]; 
 
-        HashMap<Character,Integer> Shash = new HashMap<>();
-        HashMap<Character,Integer> Thash = new HashMap<>();
 
-        for(int i=0 ; i < n ; i++){
-            Shash.put(s.charAt(i) , i);
-             Thash.put(t.charAt(i) , i);
+        for (int i = 0; i < n; i++) {
+            sIndex[s.charAt(i) - 'a'] = i; 
+            tIndex[t.charAt(i) - 'a'] = i; 
         }
 
-        for(char c:s.toCharArray()){
-            diff += (Math.abs(Shash.get(c) - Thash.get(c)));
+
+        for (char c : s.toCharArray()) {
+            diff += Math.abs(sIndex[c - 'a'] - tIndex[c - 'a']);
         }
 
-        return diff ;
-        
+        return diff;
     }
 }
