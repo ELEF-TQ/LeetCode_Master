c912237435843588a868d388e5c6ab49a96f310b
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-10-29 16:34:49 +0100

    Time: 75 ms (46.63%), Space: 45.7 MB (6.7%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,9 +1,30 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         String s2 = s + s;
-        // Enlever le premier et le dernier caractère de s2
+        /* remove first and last caracter*/
         String modified = s2.substring(1, s2.length() - 1);
-        // Vérifier si s existe dans la chaîne modifiée
         return modified.contains(s);
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean repeatedSubstringPattern(String s) {
+//         int n = s.length();
+//         for (int i = 1; i <= n / 2; i++) {
+//             if (n % i == 0) {
+//                 String sub = s.substring(0, i);
+//                 StringBuilder sb = new StringBuilder();
+//                 for (int j = 0; j < n / i; j++) {
+//                     sb.append(sub);
+//                 }
+//                 if (sb.toString().equals(s)) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false; 
+//     }
+// }
