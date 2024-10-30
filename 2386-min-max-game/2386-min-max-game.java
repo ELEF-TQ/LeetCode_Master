94da8d92b309aa6913663c7ffcc4013a0a24bae3
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-10-30 12:48:24 +0100

    Time: 1 ms (56.52%), Space: 43.5 MB (23.36%) - LeetHub

--- 2386-min-max-game/2386-min-max-game.java
+++ 2386-min-max-game/2386-min-max-game.java
@@ -2,17 +2,16 @@ class Solution {
     public int minMaxGame(int[] nums) {
         int n = nums.length;
 
+
         while (n > 1) {
-            int[] newNums = new int[n / 2];
             for (int i = 0; i < n / 2; i++) {
                 if (i % 2 == 0) {
-                    newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                    nums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
                 } else {
-                    newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                    nums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
                 }
             }
-            nums = newNums; 
-            n = nums.length; 
+            n /= 2; 
         }
         return nums[0];
     }
