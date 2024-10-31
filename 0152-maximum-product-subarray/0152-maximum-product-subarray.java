cb7962489da0147af9b896673c913582f5cc6bdd
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-10-31 21:38:11 +0100

    Time: 2 ms (56.42%), Space: 45.5 MB (8.2%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -7,18 +7,15 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
             int num = nums[i];
             
-            // Swap maxProd and minProd if the current number is negative
             if (num < 0) {
                 int temp = maxProd;
                 maxProd = minProd;
                 minProd = temp;
             }
             
-            // Calculate the maximum and minimum products up to the current element
             maxProd = Math.max(num, maxProd * num);
             minProd = Math.min(num, minProd * num);
-
-            // Update the result with the maximum product found so far
+            
             result = Math.max(result, maxProd);
         }
 
