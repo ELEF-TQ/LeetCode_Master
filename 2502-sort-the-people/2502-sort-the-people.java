08c8c2030d5f81c06b691915212aa898ea14a9c2
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-11-02 23:12:45 +0100

    Time: 8 ms (60.04%), Space: 45.3 MB (63%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -3,22 +3,19 @@ import java.util.*;
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
         int n = names.length;
+     
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+
+        //  descending order
+        Arrays.sort(indices, (a, b) -> heights[b] - heights[a]);
+
+       
         String[] sortedNames = new String[n];
-        HashMap<Integer, String> people = new HashMap<>();
-
         for (int i = 0; i < n; i++) {
-            people.put(heights[i], names[i]);
-        }
-
-        // Convert heights to Integer[] for sorting
-        Integer[] sortedHeights = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            sortedHeights[i] = heights[i];
-        }
-        Arrays.sort(sortedHeights, Collections.reverseOrder());
-
-        for (int i = 0; i < n; i++) {
-            sortedNames[i] = people.get(sortedHeights[i]);
+            sortedNames[i] = names[indices[i]];
         }
 
         return sortedNames;
