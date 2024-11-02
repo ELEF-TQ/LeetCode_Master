c38cbb1adab9414da7f393f7a08a8cd185659cd0
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-11-02 22:49:51 +0100

    Time: 1 ms (22.36%), Space: 42.1 MB (19.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        HashMap<Integer, Integer> count = new HashMap<>();
         
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        
+        int idx = 0;
+        for (int color = 0; color < 3; color++) {
+            int freq = count.getOrDefault(color, 0); 
+            for (int j = 0; j < freq; j++) {
+                nums[idx] = color;
+                idx++;
+            }
+        }        
     }
 }
