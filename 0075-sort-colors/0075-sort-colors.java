799ee7addc1c00b58e522724829b25cb2021d8ee
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-11-02 22:54:38 +0100

    Time: 0 ms (100%), Space: 41.7 MB (65.82%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,18 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        
-        for (int num : nums) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
-        }
-        
-        int idx = 0;
-        for (int color = 0; color < 3; color++) {
-            int freq = count.getOrDefault(color, 0); 
-            for (int j = 0; j < freq; j++) {
-                nums[idx] = color;
-                idx++;
+        int low = 0, i = 0, high = nums.length - 1;
+
+        while (i <= high) {
+            if (nums[i] == 0) {
+                // Swap current element with `low` pointer, then increment `low` and `i`
+                swap(nums, i, low);
+                low++;
+                i++;
+            } else if (nums[i] == 2) {
+                // Swap current element with `high` pointer, then decrement `high`
+                swap(nums, i, high);
+                high--;
+            } else {
+                // If the element is 1, just move to the next element
+                i++;
             }
-        }        
+        }
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
