1ea14491c634358ff2536f91ab8413d062609df5
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-11-11 21:17:30 +0100

    Time: 8 ms (11.08%), Space: 44.1 MB (28.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // Handle edge case for 0, which is always a palindrome
         if (x == 0) {
             return true;
         }
 
-        // Handle edge case for negative numbers
         if (x < 0) {
             return false;
         }
@@ -13,14 +11,14 @@ class Solution {
         StringBuilder rev = new StringBuilder();
         int num = x;
 
-        // Reverse the digits of the number
+      
         while (num != 0) {
             int rest = num % 10;
             rev.append(rest);
             num = num / 10;
         }
 
-        // Compare the original number with the reversed string
+
         return Integer.toString(x).equals(rev.toString());
     }
 }
