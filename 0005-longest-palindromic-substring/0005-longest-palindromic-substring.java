6120b673d2ad721814819e6f8c94be94d0c7547f
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2024-11-12 13:53:54 +0100

    Time: 17 ms (68.13%), Space: 44.6 MB (60.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,20 +4,21 @@ class Solution {
 
         String maxStr = "";
         
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j <= 1; j++) {
-                int left = i, right = i + j; // Check both odd and even length palindromes
-                while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                    left--;
-                    right++;
-                }
+      for(int i=0 ; i < s.length() ; i++){
+        for(int j=0 ; j <= 1 ; j++){
+            int left = i , right = i+j ;
 
-                // left and right have moved one extra step outside
-                if (right - left - 1 > maxStr.length()) {
-                    maxStr = s.substring(left + 1, right);
-                }
+
+            while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                left-- ;
+                right++ ;
+            }
+
+            if(right-left-1 > maxStr.length()){
+                maxStr = s.substring(left+1,right);
             }
         }
+      }
         
         return maxStr;
     }
