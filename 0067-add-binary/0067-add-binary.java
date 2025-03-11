d716ce6e5b4ce0899fde078753965ea40737382b
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-03-11 09:02:42 +0000

    Time: 1 ms (99.69%), Space: 42.5 MB (40.58%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,23 +1,21 @@
-class Solution {
-    public String addBinary(String a, String b) {
-        StringBuilder result = new StringBuilder();
-        int aLength = a.length() - 1;
-        int bLength = b.length() - 1;
-        int carry = 0;
+class Solution 
+{
+  public String addBinary(String a, String b) 
+  {
+    StringBuilder sb = new StringBuilder();
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
 
-        while (aLength >= 0 || bLength >= 0 || carry != 0) {
-
-            int bitA = (aLength >= 0) ? a.charAt(aLength) - '0' : 0;
-            int bitB = (bLength >= 0) ? b.charAt(bLength) - '0' : 0;
-            
-            int sum = bitA + bitB + carry;
-            result.append(sum % 2);
-            carry = sum / 2;
-
-            aLength--;
-            bLength--;
-        }
-
-        return result.reverse().toString();
+    while (i >= 0 || j >= 0 || carry == 1) 
+    {
+      if(i >= 0)
+        carry += a.charAt(i--) - '0';
+      if(j >= 0)
+        carry += b.charAt(j--) - '0';
+      sb.append(carry % 2);
+      carry /= 2;
     }
+    return sb.reverse().toString();
+  }
 }
