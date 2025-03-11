906d52944943fc42566be5a714d8e47b008c8ba6
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-03-11 09:26:32 +0000

    Time: 1 ms (99.83%), Space: 46.1 MB (71.56%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-
-        int res = 0 ;
-        for(int num:nums){
-            res ^= num ; // XOR : all duplicates will be eliminated
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; 
         }
-        return res ;
-        
+        return result;
     }
 }
