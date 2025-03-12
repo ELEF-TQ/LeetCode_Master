a9eb575739f2b09c395ca12e037193ac0ab5224d
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-03-12 10:42:12 +0000

    Time: 16 ms (23.5%), Space: 45.4 MB (7.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,24 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
 
-        if(s.length() != t.length()) return false ;
+        Map<Character, Integer> map = new HashMap<>();
 
-        HashMap<Character,Integer> characters = new HashMap<>();
-
+    
         for (char c : s.toCharArray()) {
-            characters.put(c, characters.getOrDefault(c, 0) + 1); 
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        
+
+   
         for (char c : t.toCharArray()) {
-            if (!characters.containsKey(c) || characters.get(c) == 0) {
-                return false; 
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;
             }
-            characters.put(c, characters.get(c) - 1);
+            map.put(c, map.get(c) - 1);
         }
 
-        return true ;
-
-      
-      
+        return true;
     }
 }
