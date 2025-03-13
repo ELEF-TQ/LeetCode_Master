dcad25f04e8cc388e6f256da92a24f13954825c4
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-03-13 11:28:56 +0000

    Time: 2 ms (99.08%), Space: 42.7 MB (95.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
-        StringBuilder sentence = new StringBuilder();
-
-        for(char c : s.toCharArray()) { 
-            if(Character.isLetterOrDigit(c)) {
-                sentence.append(Character.toLowerCase(c));
-            }
-        }
-
-
-        int left = 0;
-        int right = sentence.length() - 1;
+        int left = 0, right = s.length() - 1;
         
-        while(left < right) {
-            if(sentence.charAt(left) != sentence.charAt(right)) {
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
+            
             left++;
             right--;
         }
-
+        
         return true;
     }
 }
