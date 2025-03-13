2b21b41aa42f983574ff172c93bf0e8f789059ed
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-03-13 12:01:25 +0000

    Time: 2 ms (92.7%), Space: 47.8 MB (6.15%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
+        int left = numbers.length - 1 ;
+        int right = 0 ;
 
-        while (left <= right) {
-            int sum = numbers[left] + numbers[right];
-            if (sum > target) {
-                right--;
-            } else if (sum < target) {
-                left++;
-            } else {
-                return new int[] { left + 1, right + 1 };
-            }
+        while(right < left){
+            int sum = numbers[right] + numbers[left] ;
+            if(sum > target) left-- ;
+            else if(sum < target) right ++ ;
+            else return new int[] {right + 1, left + 1};
         }
-        
         throw new IllegalArgumentException("No two sum solution");
     }
 }
