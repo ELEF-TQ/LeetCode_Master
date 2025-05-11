cb88ec47908fe07a9b845d90daf7501a2fb9b427
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-05-11 15:24:36 +0100

    Time: 25 ms (57.97%), Space: 45.5 MB (41.64%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,23 +1,23 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean checkValid(int[][] matrix) {
+        int n = matrix.length;
 
-       int n = matrix.length ;
+        for (int i = 0; i < n; i++) {
+            HashSet<Integer> rowSet = new HashSet<>();
+            HashSet<Integer> colSet = new HashSet<>();
 
-       for(int i=0 ; i < n ; i++){
-          HashSet<Integer> rowSet = new HashSet<>();
-          HashSet<Integer> colSet = new HashSet<>();
+            for (int j = 0; j < n; j++) {
+                int rowVal = matrix[i][j];
+                int colVal = matrix[j][i];
 
-          for(int j=0 ; j < n ; j++){
-            // valid row
-            if(!rowSet.add(matrix[i][j]) || matrix[i][j] > n || matrix[i][j] < 1) return false ;
+                // if the value is outside the limit or already exists in set -> return false
+                if (rowVal < 1 || rowVal > n || !rowSet.add(rowVal)) return false;
+                if (colVal < 1 || colVal > n || !colSet.add(colVal)) return false;
+            }
+        }
 
-            // valid column
-            if(!colSet.add(matrix[j][i]) || matrix[j][i] > n || matrix[j][i] < 1) return false ;
-
-          }
-       }
-
-       return true ;
-        
+        return true;
     }
 }
