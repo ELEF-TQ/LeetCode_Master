9334ad0ca78d256532cdce977403ab28d60cfa01
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-12-30 10:24:43 +0100

    Time: 5 ms (73.94%), Space: 45.9 MB (69.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if (x < 0) return false;
 
-        String str = String.valueOf(x);
-        int left = 0;
-        int right = str.length() - 1;
+        int original = x;
+        long reversed = 0;
 
-        while(left < right){
-            if(str.charAt(left) != str.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
-        return true;
-        
+
+        return original == reversed;
     }
 }
