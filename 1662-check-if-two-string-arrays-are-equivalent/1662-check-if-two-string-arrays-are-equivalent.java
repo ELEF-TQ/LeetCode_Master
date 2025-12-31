0f0999eda2ffd00eaecd7d981bc261283924de04
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-12-31 16:33:26 +0100

    Time: 1 ms (85.58%), Space: 43.4 MB (37.86%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,26 +1,16 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        int i = 0, j = 0, p1 = 0, p2 = 0;
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
 
-        while (i < word1.length && j < word2.length) {
-            if (word1[i].charAt(p1) != word2[j].charAt(p2)) {
-                return false;
-            }
-
-            p1++;
-            p2++;
-
-            if (p1 == word1[i].length()) {
-                i++;
-                p1 = 0;
-            }
-
-            if (p2 == word2[j].length()) {
-                j++;
-                p2 = 0;
-            }
+        for (String w : word1) {
+            sb1.append(w);
         }
 
-        return i == word1.length && j == word2.length;
+        for (String w : word2) {
+            sb2.append(w);
+        }
+
+        return sb1.toString().equals(sb2.toString());
     }
 }
