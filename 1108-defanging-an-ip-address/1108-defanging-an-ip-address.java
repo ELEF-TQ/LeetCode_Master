e7bb520a2c415eef480b9cb4aa5e094b441cb7f4
TARIQ AIT LAKHLIFT <tariq75aitlakhlift@gmail.com>
2025-12-31 15:46:29 +0100

    Time: 0 ms (100%), Space: 43.2 MB (11.97%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,5 +1,16 @@
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replace(".", "[.]");
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : address.toCharArray()) {
+            if (c == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+        // return address.replace(".","[.]");
     }
 }
